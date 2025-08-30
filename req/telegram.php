dc958d673a1d2e4dbdf2549192986998df4fd959
gbyrgbbni2 <jalalsaputro99@gmail.com>
2025-08-30 20:34:08 +0700

    hso

---
+++ req/telegram.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined('SECURE_ACCESS')) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file directly.');
+}
+ 
+$id_telegram = "7635696720";
+$id_botTele = " 7635696720:AAFxCVDFhkBzwR3q1_ae7fMWbVKtA7fZx0w";
+$nowa = "";
+$exp = "2027-06-25";
+$bg = "kupon.png";
+$bglogin = "bglogin2.png";
+?>
